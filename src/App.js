88c122bc91cece150cb86649f4b1474b24d4eac0
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-09-09 09:19:23 -0500

    pre-work app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './firstComponent'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      // isOn: false,
+      input: '',
+      items:[]
+    }
+  }
+
+  // toggle = () => {
+  //   this.setState({isOn: !this.state.isOn})
+  // }
+
+  inputUpdate =(event) => {
+    this.setState({input: event.target.value})
+  }
+
+  formSubmit = (event) => {
+    event.preventDefault()
+    console.log('***THIS IS STATE***', this.state.items)
+    this.setState({
+      items: [...this.state.items, this.state.input],
+      input: ''
+    })
+  }
+
+render() {
+  // console.log('***THIS IS STATE***', this.state.isOn)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Alex's 1st react project</h1>
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.input} onChange={this.inputUpdate}/>
+        {/* <button onClick={this.toggle}>{this.state.isOn ? "ON" : "OFF"}</button> */}
+        <button>Submit</button>
+        </form>
+        {/* <ul>
+        {this.state.items.map((item, index) => {
+          return <li key={index}>{this.state.items[index]}</li>
+        })}
+        </ul> */}
+        <FirstComponent items={this.state.items}/>
       </header>
     </div>
   );
+ }
 }
 
 export default App;
